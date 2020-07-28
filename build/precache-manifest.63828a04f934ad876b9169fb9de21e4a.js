a6107967ccb4bd431fb53e6baf75b0959db6f181
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-07-28 17:37:20 -0300

    build start

---
+++ build/precache-manifest.63828a04f934ad876b9169fb9de21e4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c1a8a5bdbcadd1ed722bd6227c8774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c06aecd705a7840d58d",
+    "url": "/static/js/2.b8159470.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/static/js/2.b8159470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3d006a37503a545b2c",
+    "url": "/static/js/main.a8546760.chunk.js"
+  },
+  {
+    "revision": "5eafe7b31fdd0d42f4b7",
+    "url": "/static/js/runtime-main.fe2086be.js"
+  }
+]);
